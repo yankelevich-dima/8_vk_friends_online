6a4058f199516fdb627d78cec053d64494c1a41e
yankelevich-dima <yankelevich.dima@gmail.com>
2016-09-08 16:48:04 +0300

    Added script and readme

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # 8_vk_friends_online
+
+## Usage
+
+  - using login and password: `python vk_friends_online.py`
+  - using personal access_token: `python vk_friends_online.py <path_to_access_token>`
+
+Note that if you use 2FA you need access_token.
+
+## Generating access_token
+
+  - See [vk developers page](https://vk.com/dev/authcode_flow_user?f=1.%20%D0%9E%D1%82%D0%BA%D1%80%D1%8B%D1%82%D0%B8%D0%B5%20%D0%B4%D0%B8%D0%B0%D0%BB%D0%BE%D0%B3%D0%B0%20%D0%B0%D0%B2%D1%82%D0%BE%D1%80%D0%B8%D0%B7%D0%B0%D1%86%D0%B8%D0%B8) - Paragraph 1.
--- vk_friends_online.py
+++ vk_friends_online.py
@@ -1,32 +1,69 @@
+import sys
+import getpass
+
 import vk
 
+APP_ID = 5114432
 
-APP_ID = -1  # чтобы получить app_id, нужно зарегистрировать своё приложение на https://vk.com/dev
+
+def get_access_token(path):
+    with open(path, 'r') as f:
+        access_token = f.read()
+    return access_token.replace('\n', '')
 
 
 def get_user_login():
-    pass
+    return input('Username: ')
 
 
 def get_user_password():
-    pass
+    return getpass.getpass()
 
 
-def get_online_friends(login, password):
-    session = vk.AuthSession(
-        app_id=APP_ID,
-        user_login=login,
-        user_password=password,
-    )
+def get_session(access_token=None, login=None, password=None):
+    if access_token:
+        try:
+            session = vk.Session(
+                access_token=access_token
+            )
+        except vk.exceptions.VkAPIError:
+            print('Something wrong with access token')
+    elif login and password:
+        try:
+            session = vk.AuthSession(
+                app_id=APP_ID,
+                user_login=login,
+                user_password=password,
+            )
+        except vk.exceptions.VkAuthError:
+            print('Login or password are incorrect or this login uses 2FA')
+
+    return session or None
+
+
+def get_online_friends(session):
     api = vk.API(session)
-    # например, api.friends.get()
+    friends = api.friends.get()
+    return filter(
+        lambda friend: friend['online'],
+        api.users.get(user_ids=friends, fields=['online'])
+    )
 
 
 def output_friends_to_console(friends_online):
-    pass
+    for friend in friends_online:
+        print('{} {}'.format(friend['last_name'], friend['first_name']))
+
 
 if __name__ == '__main__':
-    login = get_user_login()
-    password = get_user_password()
-    friends_online = get_online_friends(login, password)
-    output_friends_to_console(friends_online)
+    if len(sys.argv) > 1:
+        access_token = get_access_token(sys.argv[1])
+        session = get_session(access_token=access_token)
+    else:
+        login = get_user_login()
+        password = get_user_password()
+        session = get_session(login=login, password=password)
+
+    if session is not None:
+        friends_online = get_online_friends(session)
+        output_friends_to_console(friends_online)
